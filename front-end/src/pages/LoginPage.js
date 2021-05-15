c5657ddd0ff95bdae907be131510790e9b0343fd
Adam Kleinedler <adam.kleinedler@gmail.com>
2021-05-15 02:17:37 +0200

    added login & register, login is in a working state, i figured out how to get values from material-ui text-fields

---
+++ front-end/src/pages/LoginPage.js
@@ -0,0 +1,105 @@
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  button: {
+    width: "250px",
+    marginLeft:"20px"
+  },
+  textField: {
+    marginLeft: "20px",
+    width: "250px"
+  },
+  paper: {
+    width:"300px",
+    height:"250px"
+  }
+}));
+let username;
+let password;
+function setUsernameState(e){
+  username = e
+}
+
+function setPasswordState(e){
+ password = e
+}
+
+let LoginScreen = function() {
+  const classes = useStyles();
+  return (
+  <div
+  style={{
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    color: "red"
+}}
+  >
+    <Paper variant="outlined" square className={classes.paper}>
+      <Grid container spacing={1}>
+        <Toolbar>
+          <Typography variant="h5">SoleUP login Page</Typography>
+        </Toolbar>
+        <Grid item xs={12}>
+          <TextField
+            value={username}
+            onChange={(e) => {setUsernameState(e.target.value)}}
+            className={classes.textField}
+            id="standard-basic"
+            label="Username"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            value={password}
+            onChange={(e) => {setPasswordState(e.target.value)}}
+            className={classes.textField}
+            id="standard-basic"
+            label="Password"
+          />
+        </Grid>
+        <Grid item xs={16} justify="space-between">
+          <Button
+            className={classes.button}
+            variant="contained"
+            onClick={apiCall()}
+            color="primary"
+          >
+            Login
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          Don't have an account?
+          <Button size="small" onClick={registerCall()}className={useStyles.button}>
+            {" "}
+            Register
+          </Button>
+        </Grid>
+      </Grid>
+    </Paper>
+  </div>
+  );
+}
+function apiCall() {
+  //call na prihlasenie sa
+  let payload = {
+    username: username,
+    password: password,
+    //id?
+    //
+  }
+}
+function registerCall() {
+  //registracia
+}
+export default LoginScreen;
